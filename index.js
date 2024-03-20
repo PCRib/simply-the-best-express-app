eb1e3b267e6e1a0c4f273d1fea05650db9541320
PCRib <paulocesarribeiro89@gmail.com>
2024-03-20 13:54:01 -0400

    add cors to server

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
+const cors = require('cors');
 
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5050;
 
+app.use(cors());
 app.use(express.json());
 
 const warehouseRoutes = require("./routes/warehouseRoute");
